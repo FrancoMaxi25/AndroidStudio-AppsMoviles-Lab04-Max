ee755e639f1cb13cf08ee4f9311ea649a265dab5
Maxi Franco <maxifranko40@gmail.com>
2025-09-12 16:53:25 -0500

    Añadido botón con evento onClick

--- app/src/main/java/com/example/lab04maxi/MainActivity.kt
+++ app/src/main/java/com/example/lab04maxi/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04maxi.ui.theme.Lab04MaxiTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +35,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "Hello $name!")
+
+        Button(onClick = { println("Botón presionado!") }) {
+            Text("Presióname")
+        }
+    }
 }
 
 @Preview(showBackground = true)
