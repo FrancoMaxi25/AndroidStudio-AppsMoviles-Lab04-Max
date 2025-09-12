30c2101dfada4c7e4c754be4ce76d5229a7c23d0
Maxi Franco <maxifranko40@gmail.com>
2025-09-12 17:03:20 -0500

    Añadida imagen en el layout

--- app/src/main/java/com/example/lab04maxi/MainActivity.kt
+++ app/src/main/java/com/example/lab04maxi/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab04maxi.ui.theme.Lab04MaxiTheme
@@ -41,6 +44,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Button(onClick = { println("Botón presionado!") }) {
             Text("Presióname")
         }
+
+        Image(
+            painter = painterResource(id = R.drawable.goten),
+            contentDescription = "MI IMAGEN",
+            modifier = Modifier.size(100.dp)
+        )
     }
 }
 
